e959e275055ad1dc2b7ed8291ddeb0f24b9b2bf8
eiyooooo <huxxx1234@gmail.com>
2024-02-28 23:37:51 +0800

    优化 悬浮窗权限检测

--- easycontrol/app/src/main/java/top/eiyooooo/easycontrol/app/MainActivity.java
+++ easycontrol/app/src/main/java/top/eiyooooo/easycontrol/app/MainActivity.java
@@ -2,16 +2,18 @@ package top.eiyooooo.easycontrol.app;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.widget.Toast;
+import android.view.LayoutInflater;
 
 import java.util.UUID;
 
 import top.eiyooooo.easycontrol.app.databinding.ActivityMainBinding;
+import top.eiyooooo.easycontrol.app.databinding.ItemRequestPermissionBinding;
 import top.eiyooooo.easycontrol.app.entity.AppData;
 import top.eiyooooo.easycontrol.app.entity.Device;
 import top.eiyooooo.easycontrol.app.helper.DeviceListAdapter;
@@ -36,6 +38,13 @@ public class MainActivity extends Activity {
     PublicTools.setLocale(this);
     mainActivity = ActivityMainBinding.inflate(this.getLayoutInflater());
     setContentView(mainActivity.getRoot());
+    // 检测权限
+    if (haveOverlayPermission()) startApp();
+    else createAlert();
+    super.onCreate(savedInstanceState);
+  }
+
+  private void startApp() {
     // 设置设备列表适配器、广播接收器
     deviceListAdapter = new DeviceListAdapter(this, mainActivity.devicesList);
     mainActivity.devicesList.setAdapter(deviceListAdapter);
@@ -49,34 +58,48 @@ public class MainActivity extends Activity {
     // 启动默认设备
     AppData.uiHandler.postDelayed(myBroadcastReceiver::startDefault, 1000);
     AppData.uiHandler.postDelayed(myBroadcastReceiver::startDefaultUSB, 2000);
-    super.onCreate(savedInstanceState);
-  }
-
-  @Override
-  protected void onResume() {
-    // 检查权限
-    checkPermission();
-    super.onResume();
   }
 
   @Override
   protected void onDestroy() {
-    myBroadcastReceiver.unRegister(this);
+    if (haveOverlayPermission()) myBroadcastReceiver.unRegister(this);
     super.onDestroy();
   }
 
   // 检查权限
-  private void checkPermission() {
+  private boolean haveOverlayPermission() {
     // 检查悬浮窗权限，防止某些设备如鸿蒙不兼容
     try {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+      return Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this);
+    } catch (Exception ignored) {
+      return true;
+    }
+  }
+
+  // 创建Client加载框
+  private void createAlert() {
+    ItemRequestPermissionBinding requestPermissionView = ItemRequestPermissionBinding.inflate(LayoutInflater.from(this));
+    requestPermissionView.buttonGoToSet.setOnClickListener(v -> {
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
         Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
         intent.setData(Uri.parse("package:$packageName"));
         startActivity(intent);
-        Toast.makeText(this, getString(R.string.main_float_permission), Toast.LENGTH_SHORT).show();
       }
-    } catch (Exception ignored) {
-    }
+    });
+    Dialog dialog = PublicTools.createDialog(this, false, requestPermissionView.getRoot());
+    dialog.show();
+    checkPermissionDelay(dialog);
+  }
+
+  // 定时检查
+  private void checkPermissionDelay(Dialog dialog) {
+    // 因为某些设备可能会无法进入设置或其他问题，导致不会有返回结果，为了减少不确定性，使用定时检测的方法
+    AppData.uiHandler.postDelayed(() -> {
+      if (haveOverlayPermission()) {
+        dialog.cancel();
+        startApp();
+      } else checkPermissionDelay(dialog);
+    }, 1000);
   }
 
   // 设置按钮监听
